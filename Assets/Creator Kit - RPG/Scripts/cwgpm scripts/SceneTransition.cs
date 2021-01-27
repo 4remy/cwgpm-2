b33f9cfbbc169ae7be2de816c01457a098710b8b
Nervana <remyboydell@gmail.com>
2021-01-26 22:17:05 -0800

    fading between scenes
    fading from white on start, and to white when changing scenes.
    
    It all went well. Tidied file management a bit but not massively.

--- Assets/Creator Kit - RPG/Scripts/cwgpm scripts/SceneTransition.cs
+++ Assets/Creator Kit - RPG/Scripts/cwgpm scripts/SceneTransition.cs
@@ -9,13 +9,45 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerMemory;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    private void Awake()
+    {
+        if(fadeInPanel != null)
+        {
+            //awake happens before anything else
+            //if the fadeInPanel doesn't already exist, instantiate it in a place
+
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            //destroy the fadein after 1 second
+            Destroy(panel, 1);
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
             playerMemory.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+            //SceneManager.LoadScene(sceneToLoad);
+        }
+        
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if(fadeOutPanel !=null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
         }
         
     }
