d9fba7df6bcc7c808b0aec87f3fed9744a0b258d
Nervana <remyboydell@gmail.com>
2021-02-12 13:58:51 -0800

    save system
    completed second part of mr tafts saving persistent data tutorial

--- Assets/Creator Kit - RPG/Scripts/cwgpm scripts/HeartManager.cs
+++ Assets/Creator Kit - RPG/Scripts/cwgpm scripts/HeartManager.cs
@@ -21,8 +21,11 @@ public class HeartManager : MonoBehaviour
     {
         for(int i = 0; i < heartContainers.initialValue; i ++)
         {
-            hearts[i].gameObject.SetActive(true);
-            hearts[i].sprite = fullHeart;
+            if(i < hearts.Length)
+            {
+                hearts[i].gameObject.SetActive(true);
+                hearts[i].sprite = fullHeart;
+            }
         }
     }
     public void UpdateHearts()
