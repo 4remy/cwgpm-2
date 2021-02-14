fd62058461c7524dd93f8b05a48f649ae601a871
Nervana <remyboydell@gmail.com>
2021-02-14 00:21:16 -0800

    Inventory pt 1 done
    The problem is a conflict with the rpgm namespace. so the tutorial is the same but its called like inventory2 right now.

---
+++ Assets/Creator Kit - RPG/Scripts/cwgpm scripts/Inventory2/Inventory2Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory2/Items")]
+public class Inventory2Item : ScriptableObject
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemImage;
+    public Sprite itemImageClose;
+    public int numberHeld;
+    public bool usable;
+    public bool unique;
+}
