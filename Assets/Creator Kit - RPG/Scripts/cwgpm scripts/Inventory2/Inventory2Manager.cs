08f52c5d89c27bf08cfe9c172a81ed32818a2c12
Nervana <remyboydell@gmail.com>
2021-02-17 01:37:12 -0800

    Inventory tutorial progress
    40 minute part of mr taft inventory tutorial completed

---
+++ Assets/Creator Kit - RPG/Scripts/cwgpm scripts/Inventory2/Inventory2Manager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Inventory2Manager : MonoBehaviour
+{
+    [Header("Inventory Information")]
+    public PlayerInventory2 playerInventory2;
+    [SerializeField] private GameObject blankInventorySlot;
+    [SerializeField] private GameObject inventoryPanelContent;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private GameObject useButton;
+
+    public void SetTextAndButton(string description, bool buttonActive)
+    {
+        descriptionText.text = description;
+        if(buttonActive)
+        {
+            useButton.SetActive(true);
+        }
+        else
+        {
+            useButton.SetActive(false);
+        }
+    }
+
+    void MakeInventorySlots()
+    {
+        if(playerInventory2)
+        {
+            for(int i = 0; i < playerInventory2.myInventory.Count; i++)
+            {
+                GameObject temp =
+                    Instantiate(blankInventorySlot, inventoryPanelContent.transform.position, Quaternion.identity);
+                temp.transform.SetParent(inventoryPanelContent.transform);
+                InventorySlot newSlot = temp.GetComponent<InventorySlot>();
+                if(newSlot)
+                {
+                    newSlot.Setup(playerInventory2.myInventory[i], this);
+                }
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeInventorySlots();
+        SetTextAndButton("", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Creator Kit - RPG/Scripts/cwgpm scripts/Inventory2/InventorySlot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [Header("UI Stuff to change")]
+    [SerializeField] private TextMeshProUGUI itemNumberText;
+    [SerializeField] private Image itemImage;
+    // [SerializeField] private Image CloseImage;
+
+    [Header("Variables from the item")]
+    //public Sprite itemSprite;
+    //    public Sprite closeSprite;
+   // public int numberHeld;
+    public string itemDescription;
+    public Inventory2Item thisItem;
+    public Inventory2Manager thisManager;
+
+    public void Setup(Inventory2Item newItem, Inventory2Manager newManager)
+    {
+        thisItem = newItem;
+        thisManager = newManager;
+        if(thisItem)
+        {
+            itemImage.sprite = thisItem.itemImage;
+            itemNumberText.text = "" + thisItem.numberHeld;
+        }
+    }
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
