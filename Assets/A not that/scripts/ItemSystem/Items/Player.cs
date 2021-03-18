23dd75777244908b2c314fe9ca6703c0e193f5fc
Nervana <jeremyboydell@gmail.com>
2021-03-17 21:30:58 -0700

    Item Drop added
    Burger can be picked up. Make the item with a gameobject, spritesheet, 2D collider with trigger. apply itemdrop. apply the scriptable object version of the item, to 'itemdrop' in world.

---
+++ Assets/A not that/scripts/ItemSystem/Items/Player.cs
@@ -0,0 +1,8 @@
+﻿using Schwer.ItemSystem;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Schwer.ItemSystem.InventorySO _inventory = default;
+    public Schwer.ItemSystem.Inventory inventory => _inventory.value;
+}
