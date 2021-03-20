0496768d9ec6da7761a171285ada3f562e4741e4
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-03-20 17:13:49 +1100

    Clean up ItemDrop.cs

--- Assets/A not that/schwer-scripts-master/SchwerScripts/ItemSystem/ItemDrop.cs
+++ Assets/A not that/schwer-scripts-master/SchwerScripts/ItemSystem/ItemDrop.cs
@@ -1,17 +1,16 @@
-﻿using Schwer.ItemSystem;
-using UnityEngine;
+﻿using UnityEngine;
 
-    public class ItemDrop : MonoBehaviour
+public class ItemDrop : MonoBehaviour
+{
+    [SerializeField] private Schwer.ItemSystem.Item item = default;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        [SerializeField] private Schwer.ItemSystem.Item item = default;
-
-        private void OnTriggerEnter2D(Collider2D other)
+        var player = other.GetComponent<Player>();
+        if (player != null)
         {
-            var player = other.GetComponent<Player>();
-            if (player != null)
-            {
-                player.inventory[item]++;
-                Destroy(this.gameObject);
-            }
+            player.inventory[item]++;
+            Destroy(this.gameObject);
         }
     }
+}
