e754771f213c4730f4595a091b30f6bed415f1bc
Nervana <jeremyboydell@gmail.com>
2021-03-20 22:37:04 -0700

    sprite changer created
    a random sprite will be set ONCE. this is the sprite changer script. there is commented out stuff that changes it on click.

---
+++ Assets/A not that/scripts/cwgpm scripts/randomChange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomChange : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    //public Sprite newSprite;
+    public Sprite[] spriteArray;
+    private bool spriteChanged = false;
+    /*void Update()
+    {
+         change sprite when clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeSprite(newSprite);
+        }
+        
+    }*/
+    void Update()
+    {
+        if (!spriteChanged)
+        {
+            ChangeSprite();
+        }
+       
+    }
+    void ChangeSprite()
+    {
+        spriteRenderer.sprite = spriteArray[UnityEngine.Random.Range(0, spriteArray.Length)];
+        spriteChanged = true;
+    }
+}
