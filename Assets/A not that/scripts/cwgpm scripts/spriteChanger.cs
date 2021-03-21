efba91d0f37fdce8ddea864d8dc17e374e3b689a
Nervana <jeremyboydell@gmail.com>
2021-03-20 23:10:29 -0700

    sprite changer improved
    sprites can be assigned randomly out of a list, only once, on start.
    
    I mean, you could get it to happen randomly every however many seconds if you wanted, I guess you would use a coroutine for that though.
    
    You can also set a specific sprite. At the moment, this gets set in the update method.  But my idea is that something outside of this script could call it.
    
    Eventually I was thinking of an enum for like, emotion states, and something that sets it externally, for NPC for example.

---
+++ Assets/A not that/scripts/cwgpm scripts/spriteChanger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BehaviourType
+{
+    random,
+    whenClicked,
+    specific
+}
+
+public class spriteChanger : MonoBehaviour
+{
+    public BehaviourType thisBehaviourType;
+
+    public SpriteRenderer spriteRenderer;
+    //public Sprite newSprite;
+    public Sprite[] spriteArray;
+    private bool spriteChanged = false;
+    public Sprite clickedSprite;
+    public Sprite specificSprite;
+    /*void Update()
+    {
+         change sprite when clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeSprite(newSprite);
+        }
+        
+    }*/
+    void Update()
+    {
+        if (thisBehaviourType == BehaviourType.random)
+        {
+            if (!spriteChanged)
+            {
+                RandomChange();
+            }
+        }
+
+        if (thisBehaviourType == BehaviourType.whenClicked)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (!spriteChanged)
+                {
+                    ClickedSprite();
+                }
+            }
+        }
+
+        if (thisBehaviourType == BehaviourType.specific)
+        {
+            //the lack of sprite changed means this can happen a lot
+            SetSpecific();
+        }
+    }
+    void RandomChange()
+    {
+        spriteRenderer.sprite = spriteArray[UnityEngine.Random.Range(0, spriteArray.Length)];
+        spriteChanged = true;
+    }
+
+    void ClickedSprite()
+    {
+        //changes to 'clicked sprite'
+        spriteRenderer.sprite = clickedSprite;
+        //locks after doing this once
+        spriteChanged = true;
+        //update method will look for if spriteChanged = true before doing anything 
+    }
+
+    void SetSpecific()
+    {
+        spriteRenderer.sprite = specificSprite;
+    }
+}
