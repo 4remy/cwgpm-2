a17c50a719861e21857d50fe15b3547a8b85f67d
Nervana <jeremyboydell@gmail.com>
2021-03-29 00:29:58 -0700

    Prior required conversation
    conversation item can be attached that requires a different conversation to have been completed in the past.
    
    have not tested: two conversations placed on one NPC, with one dependent on the other. If this is an issue, a coroutine taking place for dependent conversations might be necessary.
    
    Mild issues with getting the 'heart' script on DropItem.cs items - how can I make it usable, when using the item forces me to point to a game object for a script (eg health up script). When we pick up an item, it gets destroyed.
    
    (It seems like a destroyed item, i wouldnt be able to use the code on it, since it's destroyed.)

--- Assets/cwg/scripts/cwgpm scripts/ScriptableObjects/Dialogs/DialogNPC.cs
+++ Assets/cwg/scripts/cwgpm scripts/ScriptableObjects/Dialogs/DialogNPC.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public enum NPCType
+public enum SpeechType
 {
-    loopingTalker,
-    linearTalker
+    loopingConvo,
+    //change to loopingConvo
+    linearConvo
+        //change to oneOffConvo
+        //add dependentConvo
+        //then check the the required conversation boolvalue is true, otherwise print a debug log that it the required convo hadn't been completeld
 }
 
 public class DialogNPC : Interactable
@@ -13,15 +17,58 @@ public class DialogNPC : Interactable
     [SerializeField] private Conversation conversation;
     public bool isCompleted;
     public BoolValue convoCompleted;
-    public NPCType thisNPCType;
+    public SpeechType thisSpeechType;
+    //do you want your conversation to pre-require another convo before it?
+    [Header("Requirements")]
+    public bool requiresPrior;
+    //stick something random in here,
+    //when you don't have this turned on
+    //because I don't know how to remove need for these
+    //when requiresBool is turned off.
+    public bool requiredToBeCompleted;
+    public BoolValue requiredCompleted;
+
+    //how do you make it wait a coroutine if it requires a prior, and the prior is also on the same character
+    //maybe it would work good 2 convos on same character
 
     private void Start()
     {
         isCompleted = convoCompleted.RuntimeValue;
+        requiredToBeCompleted = requiredCompleted.RuntimeValue;
+
     }
     protected override void Interact()
     {
-        if (thisNPCType == NPCType.linearTalker)
+        if (requiresPrior)
+        {
+            if(requiredToBeCompleted)
+            {
+                if (thisSpeechType == SpeechType.linearConvo)
+                {
+                    if (!isCompleted)
+                    {
+                        Debug.Log("interacting");
+                        DialogDisplay.NewConversation(conversation);
+                        isCompleted = true;
+                        convoCompleted.RuntimeValue = isCompleted;
+                    }
+                    else
+                    {
+                        Debug.Log("finished Talking");
+                    }
+                }
+                else
+                {
+                    Debug.Log("interacting");
+                    DialogDisplay.NewConversation(conversation);
+                }
+            }
+            Debug.Log("hasn't completed required prior conversation");
+        }
+        else
+        //check BoolValue for convoCompleted is true
+        //if it is, do the below
+        if (thisSpeechType == SpeechType.linearConvo)
         {
             if (!isCompleted)
             {
@@ -39,7 +86,7 @@ public class DialogNPC : Interactable
 
 
        //
-       //else (thisNPCType == NPCType.loopingTalker)
+       //else (thisSpeechType == SpeechType.loopingConvo)
        else
         {
             Debug.Log("interacting");
@@ -49,5 +96,6 @@ public class DialogNPC : Interactable
     }
 
 
+    //Debug.Log("hasn't completed required prior conversation");
 }
 
