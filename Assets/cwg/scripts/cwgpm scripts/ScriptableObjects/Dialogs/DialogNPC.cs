5f6b9d7e1cece5e6c513da98283686c432248ea9
Nervana <jeremyboydell@gmail.com>
2021-03-28 22:49:06 -0700

    NPC talking styles
    character can talk 'on Loop' or talk in Linear manner, eg with one single conversation they don't repeat. Stored as 'conversation completed boolValue'

--- Assets/cwg/scripts/cwgpm scripts/ScriptableObjects/Dialogs/DialogNPC.cs
+++ Assets/cwg/scripts/cwgpm scripts/ScriptableObjects/Dialogs/DialogNPC.cs
@@ -1,13 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum NPCType
+{
+    loopingTalker,
+    linearTalker
+}
 
 public class DialogNPC : Interactable
 {
     //reference to the NPC's dialog
     [SerializeField] private Conversation conversation;
+    public bool isCompleted;
+    public BoolValue convoCompleted;
+    public NPCType thisNPCType;
 
+    private void Start()
+    {
+        isCompleted = convoCompleted.RuntimeValue;
+    }
     protected override void Interact()
     {
-        Debug.Log("interacting");
-        DialogDisplay.NewConversation(conversation);
+        if (thisNPCType == NPCType.linearTalker)
+        {
+            if (!isCompleted)
+            {
+                Debug.Log("interacting");
+                DialogDisplay.NewConversation(conversation);
+                isCompleted = true;
+                convoCompleted.RuntimeValue = isCompleted;
+            }
+            else
+            {
+                Debug.Log("finished Talking");
+            }
+        }
+
+
+
+       //
+       //else (thisNPCType == NPCType.loopingTalker)
+       else
+        {
+            Debug.Log("interacting");
+            DialogDisplay.NewConversation(conversation);
+        }
+
     }
+
+
 }
+
