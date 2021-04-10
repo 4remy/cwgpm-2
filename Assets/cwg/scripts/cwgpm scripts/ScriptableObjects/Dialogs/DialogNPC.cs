59a7212ce4f32ea4d23fa1e75c4aaaf455ea9a46
Nervana <jeremyboydell@gmail.com>
2021-04-10 03:06:10 -0700

    dialogNPC changes
    but keeping Schwer's changes to randomScript

--- Assets/cwg/scripts/cwgpm scripts/ScriptableObjects/Dialogs/DialogNPC.cs
+++ Assets/cwg/scripts/cwgpm scripts/ScriptableObjects/Dialogs/DialogNPC.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public enum SpeechType
 {
@@ -11,25 +12,57 @@ public enum SpeechType
         //then check the the required conversation boolvalue is true, otherwise print a debug log that it the required convo hadn't been completeld
 }
 
+#if UNITY_EDITOR
+#endif
+
 public class DialogNPC : Interactable
 {
+    [Header("Requirements")]
+   // HideInInspector makes sure the default inspector won't show these fields.
+    public bool requiresPrior;
+    [HideInInspector]
+    public bool requiredToBeCompleted;
+    public BoolValue requiredCompleted;
+
+    [HideInInspector]
+    public GameObject Template;
+
+
     //reference to the NPC's dialog
     [SerializeField] private Conversation conversation;
     public bool isCompleted;
     public BoolValue convoCompleted;
     public SpeechType thisSpeechType;
     //do you want your conversation to pre-require another convo before it?
-    [Header("Requirements")]
-    public bool requiresPrior;
-    //stick something random in here,
-    //when you don't have this turned on
-    //because I don't know how to remove need for these
-    //when requiresBool is turned off.
-    public bool requiredToBeCompleted;
-    public BoolValue requiredCompleted;
+
+
+
+
 
     //how do you make it wait a coroutine if it requires a prior, and the prior is also on the same character
     //maybe it would work good 2 convos on same character
+
+#if UNITY_EDITOR
+ [CustomEditor(typeof(DialogNPC))]
+ public class DialogNPC_Editor : Editor
+ {
+     public override void OnInspectorGUI()
+     {
+         DrawDefaultInspector(); // for other non-HideInInspector fields
+ 
+         DialogNPC script = (DialogNPC)target;
+ 
+         // draw checkbox for the bool
+         script.requiresPrior = EditorGUILayout.Toggle("Requires Prior", script.requiresPrior);
+         if (script.requiresPrior) // if bool is true, show other fields
+         {
+         //requires reference for non static
+         //this line below is broken
+            DialogNPC.requiredCompleted.BoolValue = EditorGUILayout.ObjectField("requiredCompleted", DialogNPC.requiredCompleted, typeof(ScriptableObject));
+         }
+     }
+ }
+#endif
 
     private void Start()
     {
