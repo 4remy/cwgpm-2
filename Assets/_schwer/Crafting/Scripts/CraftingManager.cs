ff731b832f7b48bd0762ea7180c273ff284358a5
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-05 19:03:32 +1000

    Create CraftingManager.cs

---
+++ Assets/_schwer/Crafting/Scripts/CraftingManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Schwer.ItemSystem.Demo;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Schwer.ItemSystem
+{
+    public class CraftingManager : MonoBehaviour, IItemSlotManager {
+        [SerializeField] private InventorySO _inventory = default;
+        private Inventory inventory => _inventory.value;
+
+        [Header("Components")]
+        [SerializeField] private Text nameDisplay = default;
+        [SerializeField] private GameObject addButton = default;
+        [SerializeField] private GameObject clearButton = default;
+
+        [Header("Containers")]
+        [SerializeField] private GameObject inventorySlotsHolder = default;
+        [SerializeField] private GameObject ingredientSlotsHolder = default;
+
+        private List<ItemSlot> inventorySlots = new List<ItemSlot>();
+
+        private Inventory ingredients = new Inventory();
+        private List<ItemSlot> ingredientSlots = new List<ItemSlot>();
+
+        private Item selectedItem;
+
+        private void OnEnable() {
+            inventory.OnContentsChanged += UpdateInventorySlots;
+
+            var selected = EventSystem.current.currentSelectedGameObject;
+            if (selected == null || !selected.transform.IsChildOf(this.transform)) {
+                EventSystem.current.SetSelectedGameObject(inventorySlots[0].gameObject);
+            }
+
+            Initialise();
+        }
+
+        private void OnDisable() => inventory.OnContentsChanged -= UpdateInventorySlots;
+
+        private void Awake() {
+            inventorySlotsHolder.GetComponentsInChildren<ItemSlot>(inventorySlots);
+            ingredientSlotsHolder.GetComponentsInChildren<ItemSlot>(ingredientSlots);
+
+            foreach (var slot in inventorySlots) {
+                slot.manager = this;
+            }
+        }
+
+        private void Initialise() {
+            OnItemSelected(null);
+            if (inventory != null) {
+                UpdateInventorySlots(null, 0);
+                UpdateIngredientSlots();
+            }
+        }
+
+        private void UpdateInventorySlots(Item item, int count) => UpdateItemSlots(inventorySlots, inventory);
+
+        private void UpdateIngredientSlots() => UpdateItemSlots(ingredientSlots, ingredients);
+
+        private void UpdateItemSlots(List<ItemSlot> itemSlots, Inventory inventory) {
+            for (int i = 0; i < itemSlots.Count; i++) {
+                if (i < inventory.Count) {
+                    var entry = inventory.ElementAt(i);
+                    itemSlots[i].SetItem(entry.Key, entry.Value);
+                }
+                else {
+                    itemSlots[i].Clear();
+                }
+            }
+        }
+
+        public void OnItemSelected(Item item) {
+            selectedItem = item;
+
+            if (item != null) {
+                nameDisplay.text = item.name;
+                addButton.SetActive(true);
+            }
+            else {
+                nameDisplay.text = "";
+                addButton.SetActive(false);
+            }
+        }
+    }
+}
