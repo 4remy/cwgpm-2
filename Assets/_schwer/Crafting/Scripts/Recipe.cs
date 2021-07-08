9aa2bcbe9bdc44bafce7c15d69b36ac0f9389abe
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-08 14:10:56 +1000

    Create Recipe and RecipeInspector

---
+++ Assets/_schwer/Crafting/Scripts/Recipe.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Schwer.ItemSystem {
+    [CreateAssetMenu(menuName = "Item System/Crafting Recipe")]
+    public class Recipe : ScriptableObject {
+        public Item output = default;
+        [Min(1)]public int outputAmount = default;
+        [Space]
+        public Inventory input = new Inventory();
+    }
+}
