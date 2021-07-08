7b0d75b403010dc0155288017e30d627929e5ccc
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-08 14:51:29 +1000

    Update Recipe.cs

--- Assets/_schwer/Crafting/Scripts/Recipe.cs
+++ Assets/_schwer/Crafting/Scripts/Recipe.cs
@@ -4,7 +4,7 @@ namespace Schwer.ItemSystem {
     [CreateAssetMenu(menuName = "Item System/Crafting Recipe")]
     public class Recipe : ScriptableObject {
         public Item output = default;
-        [Min(1)]public int outputAmount = default;
+        [Min(1)]public int outputAmount = 1;
         [Space]
         public Inventory input = new Inventory();
 
