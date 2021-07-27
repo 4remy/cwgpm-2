fa639b39c227b908590dc657608467e286c2f79d
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-27 20:41:18 +1000

    Remove unnecessary usings

--- Assets/cwg/schwer-scripts-master/schwer-scripts/Database/ScriptableObjectDatabase.cs
+++ Assets/cwg/schwer-scripts-master/schwer-scripts/Database/ScriptableObjectDatabase.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Schwer.Database {
     public abstract class ScriptableObjectDatabase<T> : ScriptableObject where T : ScriptableObject {
