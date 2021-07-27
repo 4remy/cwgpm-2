012bcf8329d8899f895a8a8147c1bbd55431884b
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-27 20:22:25 +1000

    Refactor ItemDatabase and utility
    Inherit from new generic database classes

--- Assets/cwg/schwer-scripts-master/schwer-scripts/ItemSystem/ScriptableObjects/ItemDatabase.cs
+++ Assets/cwg/schwer-scripts-master/schwer-scripts/ItemSystem/ScriptableObjects/ItemDatabase.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Schwer.ItemSystem {
-    public class ItemDatabase : ScriptableObject {
+    using Schwer.Database;
+
+    public class ItemDatabase : ScriptableObjectDatabase<Item> {
         // Generated via ItemDatabaseUtility
-        [field:SerializeField] private List<Item> items;
-        public void Initialise(List<Item> items) {
-            this.items = items;
+        [SerializeField] private Item[] items;
+
+        public override void Initialise(Item[] items) {
+            this.items = FilterItemsByID(items);
         }
 
         public Item GetItem(int itemID) {
@@ -23,6 +27,23 @@ namespace Schwer.ItemSystem {
             }
             if (result == null) { Debug.LogWarning("Item with ID '" + itemID + "' was not found in the database."); }
             return result;
+        }
+
+        private Item[] FilterItemsByID(Item[] items) {
+            var filteredItems = new List<Item>();
+            var filteredIDs = new List<int>();
+
+            for (int i = 0; i < items.Length; i++) {
+                if (filteredIDs.Contains(items[i].id)) {
+                    var sharedID = filteredItems[filteredIDs.IndexOf(items[i].id)].name;
+                    Debug.LogWarning($"'{items[i].name}' was excluded from {this.name} because it shares its ID ({items[i].id}) with '{sharedID}'.");
+                }
+                else {
+                    filteredItems.Add(items[i]);
+                    filteredIDs.Add(items[i].id);
+                }
+            }
+            return filteredItems.OrderBy(i => i.id).ToArray();
         }
     }
 }
