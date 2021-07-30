6da49f656e27b640e579ab237db16691543222d9
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-30 16:02:39 +1000

    Update `RecipeSlot`

--- Assets/_schwer/Crafting/Scripts/RecipeSlot.cs
+++ Assets/_schwer/Crafting/Scripts/RecipeSlot.cs
@@ -5,6 +5,7 @@ namespace Schwer.ItemSystem {
     public class RecipeSlot : MonoBehaviour {
         [Header("Components")]
         [SerializeField] private Image sprite = default;
+        [SerializeField] private Text text = default;
         [SerializeField] private Button _button = default;
         public Button button => _button;
 
@@ -13,11 +14,13 @@ namespace Schwer.ItemSystem {
         public void Initialise(Recipe recipe, bool discovered) {
             this.recipe = recipe;
             if (recipe != null) {
+                text.text = recipe.output.name;
                 sprite.sprite = recipe.output.sprite;
                 sprite.enabled = true;
                 sprite.color = discovered ? Color.white : Color.black;
             }
             else {
+                text.text = "";
                 sprite.enabled = false;
                 sprite.sprite = null;
             }
