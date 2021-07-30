4ee6b1c8289370fbbddada79705afc671981b537
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-07-30 14:01:52 +1000

    Create RecipeSlot

---
+++ Assets/_schwer/Crafting/Scripts/RecipeSlot.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Schwer.ItemSystem
+{
+    public class RecipeSlot : MonoBehaviour, ISelectHandler {
+        [Header("Components")]
+        [SerializeField] private Image sprite = default;
+
+        public IRecipeSlotManager manager { get; set; }
+        public Recipe recipe { get; private set; }
+
+        public void Initialise(Recipe recipe, bool discovered) {
+            this.recipe = recipe;
+            if (recipe != null) {
+                sprite.sprite = recipe.output.sprite;
+                sprite.enabled = true;
+                sprite.color = discovered ? Color.white : Color.black;
+            }
+            else {
+                sprite.enabled = false;
+                sprite.sprite = null;
+            }
+        }
+
+        public void OnSelect(BaseEventData eventData) => manager?.OnRecipeSelected(recipe);
+    }
+
+    public interface IRecipeSlotManager {
+        void OnRecipeSelected(Recipe recipe);
+    }
+}
