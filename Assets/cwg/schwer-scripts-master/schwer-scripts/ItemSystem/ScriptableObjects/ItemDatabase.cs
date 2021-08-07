7219716b98561d33ad29dbd0689089fed8832a35
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-08-07 21:59:02 +1000

    Move id methods

--- Assets/cwg/schwer-scripts-master/schwer-scripts/ItemSystem/ScriptableObjects/ItemDatabase.cs
+++ Assets/cwg/schwer-scripts-master/schwer-scripts/ItemSystem/ScriptableObjects/ItemDatabase.cs
@@ -10,40 +10,9 @@ namespace Schwer.ItemSystem {
         [SerializeField] private Item[] items;
 
         public override void Initialise(Item[] items) {
-            this.items = FilterItemsByID(items);
+            this.items = FilterByID(items);
         }
 
-        public Item GetItem(int itemID) {
-            Item result = null;
-            foreach (var item in items) {
-                if (item == null) {
-                    Debug.LogWarning("The database contains a null entry. Update the ItemDatabase to fix (refer to the readme for help).");
-                    continue;
-                }
-
-                if (item.id == itemID) {
-                    result = item;
-                }
-            }
-            if (result == null) { Debug.LogWarning("Item with ID '" + itemID + "' was not found in the database."); }
-            return result;
-        }
-
-        private Item[] FilterItemsByID(Item[] items) {
-            var filteredItems = new List<Item>();
-            var filteredIDs = new List<int>();
-
-            for (int i = 0; i < items.Length; i++) {
-                if (filteredIDs.Contains(items[i].id)) {
-                    var sharedID = filteredItems[filteredIDs.IndexOf(items[i].id)].name;
-                    Debug.LogWarning($"'{items[i].name}' was excluded from {this.name} because it shares its ID ({items[i].id}) with '{sharedID}'.");
-                }
-                else {
-                    filteredItems.Add(items[i]);
-                    filteredIDs.Add(items[i].id);
-                }
-            }
-            return filteredItems.OrderBy(i => i.id).ToArray();
-        }
+        public Item GetItem(int itemID) => GetFromID(itemID, items);
     }
 }
