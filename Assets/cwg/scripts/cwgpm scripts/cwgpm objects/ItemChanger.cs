d4558e7ec6fb9f263dc20fea96886294be8387d9
Nervana <jeremyboydell@gmail.com>
2021-08-22 02:24:16 -0700

    Item checker and changer
    on the cooker. can check and change items. still minor issues getting the dialog box to go away on the spacepress.

---
+++ Assets/cwg/scripts/cwgpm scripts/cwgpm objects/ItemChanger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SchwerInventory = Schwer.ItemSystem.Inventory;
+
+public class ItemChanger : Interactable
+{
+    [Header("Inventory")]
+    [SerializeField] private Schwer.ItemSystem.InventorySO _inventory = default;
+    public SchwerInventory inventory => _inventory.value;
+
+    //[Header("item to be changed")]
+    [SerializeField] private Schwer.ItemSystem.Item oldItem = default;
+
+    //[Header("New item")]
+    [SerializeField] private Schwer.ItemSystem.Item newItem = default;
+
+
+
+    //public BoxCollider2D box;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get animators here
+    }
+
+
+    protected override void Interact()
+    {
+        if (player.inventory[oldItem] > 0)
+        {
+
+            Debug.Log("you had the item I was looking for!");
+            player.inventory[oldItem]--;
+            player.inventory[newItem]++;
+            //put animation here
+        }
+        else
+        {
+            Debug.Log("item not found");
+        }
+    }
+}
---
+++ Assets/cwg/scripts/cwgpm scripts/cwgpm objects/ItemChecker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SchwerInventory = Schwer.ItemSystem.Inventory;
+
+public class ItemChecker : MonoBehaviour
+{
+    [Header("Inventory")]
+    [SerializeField] private Schwer.ItemSystem.InventorySO _inventory = default;
+    public SchwerInventory inventory => _inventory.value;
+
+    //[Header("item you are checking")]
+    [SerializeField] private Schwer.ItemSystem.Item oldItem = default;
+
+    public bool playerInRange;
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    [Multiline]
+    public string dialog;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (inventory[oldItem] > 0)
+            {
+                Debug.Log("You can cook now");
+                playerInRange = true;
+
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                      dialogBox.SetActive(false);
+                    //this bit (turning off dialog box isn't working yet
+                    //rest is all good tho
+                      Debug.Log("space pressed");
+                }
+
+            }
+            else
+            {
+                Debug.Log("fuck you no meat");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
+            playerInRange = false;
+
+            dialogBox.SetActive(false);
+
+        }
+    }
+}
