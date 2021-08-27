44d11e309c0e7d618647e46b374d45f9fc17a23b
Nervana <jeremyboydell@gmail.com>
2021-08-26 19:32:57 -0700

    'Use button'
    commented out public bool and 'use button' code in Inventory manager script in prefab

---
+++ Assets/cwg/cwgpm scripts/Giveable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Giveable : MonoBehaviour
+{
+    public bool GiveableCollided;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.CompareTag("Giveable"))
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("player in giving range");
+                GiveableCollided = true;
+            }
+        }
+
+    }
+}
--- Assets/schwer-scripts-master/schwer-scripts/ItemSystem/Demo/UI/InventoryManager.cs
+++ Assets/schwer-scripts-master/schwer-scripts/ItemSystem/Demo/UI/InventoryManager.cs
@@ -13,6 +13,11 @@ namespace Schwer.ItemSystem.Demo {
         [SerializeField] private Text nameDisplay = default;
         [SerializeField] private Text descriptionDisplay = default;
         [SerializeField] private GameObject useButton = default;
+        [SerializeField] private GameObject giveButton = default;
+        // get the public bool GiveableCollided from Giveable.cs
+        //GetComponent<Giveable>().GiveableCollided
+        // public Giveable Gvb;
+        //private bool Giveable;
 
         private List<ItemSlot> itemSlots = new List<ItemSlot>();
 
@@ -83,10 +88,25 @@ namespace Schwer.ItemSystem.Demo {
             // ∴ the active status of the use button should be
             //   handled here, since it depends on the selected item.
             useButton.SetActive((item is UsableItem));
+
+            //if player is colliding with 'givable' character
+            //find public bool
+
+            /*
+            Giveable = gameObject.GetComponent<Giveable>().GiveableCollided;
+            if (Giveable)
+            {
+                    Debug.Log("give button active??");
+                    //giveButton.SetActive;
+                
+            }
+            */
+
         }
 
         // Called by the use button's OnClick UnityEvent
-        public void UseButtonPressed() {
+        public void UseButtonPressed()
+        {
             if (selectedItem is UsableItem selectedUsable) {
                 selectedUsable.Use();
                 inventory[selectedUsable]--;
@@ -96,6 +116,14 @@ namespace Schwer.ItemSystem.Demo {
                     UpdateDisplay(null);
                 }
             }
+        }
+
+        // Called by the use button's OnClick UnityEvent
+        public void GiveButtonPressed()
+        {
+            Debug.Log("give button pressed");
+            // call the give function on the character in overworld
+            //givable.give();
         }
     }
 }
