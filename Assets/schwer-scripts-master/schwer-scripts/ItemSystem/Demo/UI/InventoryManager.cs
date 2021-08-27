eb8f54a4a4080791eb00e5ea82a61614ee90371d
Nervana <jeremyboydell@gmail.com>
2021-08-26 19:57:44 -0700

    stash2
    stash but im gonna rever

--- Assets/schwer-scripts-master/schwer-scripts/ItemSystem/Demo/UI/InventoryManager.cs
+++ Assets/schwer-scripts-master/schwer-scripts/ItemSystem/Demo/UI/InventoryManager.cs
@@ -13,7 +13,10 @@ namespace Schwer.ItemSystem.Demo {
         [SerializeField] private Text nameDisplay = default;
         [SerializeField] private Text descriptionDisplay = default;
         [SerializeField] private GameObject useButton = default;
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 44d11e30 ('Use button')
 
         private List<ItemSlot> itemSlots = new List<ItemSlot>();
 
@@ -79,13 +82,20 @@ namespace Schwer.ItemSystem.Demo {
                 descriptionDisplay.text = "";
             }
 
+<<<<<<< HEAD
 
 
+=======
+            // UpdateDisplay handles displaying information about
+            // the currently selected item.
+            // ∴ the active status of the use button should be
+            //   handled here, since it depends on the selected item.
+            useButton.SetActive((item is UsableItem));
+>>>>>>> parent of 44d11e30 ('Use button')
         }
 
         // Called by the use button's OnClick UnityEvent
-        public void UseButtonPressed()
-        {
+        public void UseButtonPressed() {
             if (selectedItem is UsableItem selectedUsable) {
                 selectedUsable.Use();
                 inventory[selectedUsable]--;
@@ -96,6 +106,9 @@ namespace Schwer.ItemSystem.Demo {
                 }
             }
         }
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 44d11e30 ('Use button')
     }
 }
