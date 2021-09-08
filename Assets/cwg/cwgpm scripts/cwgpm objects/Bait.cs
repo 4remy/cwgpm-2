b9be928b9c1b1498854a005d535bc90619e4530e
Nervana <jeremyboydell@gmail.com>
2021-09-07 17:36:25 -0700

    bait and monster script begun
    went back to the basics and tightened the script up a bit (by which i mean it works correctly on a basic level now)
    things need to be a prefab to be a use react. not sure if i want this in the final build.

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/Bait.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using SchwerInventory = Schwer.ItemSystem.Inventory;
+using UnityEngine.UI;
+
+public class Bait : MonoBehaviour
+{
+    // [SerializeField] private Schwer.ItemSystem.Item item = default;
+
+
+    //i think interact isnt working because of the triggers
+
+    public bool baitZone;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && baitZone)
+        {
+            Debug.Log("you have interacted lol");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            Debug.Log("player in bait zone");
+            baitZone = true;
+        Debug.Log("baitZoneis " + gameObject.name + " is " + baitZone);
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+            Debug.Log("player has exited bait zone");
+            baitZone = false;
+            Debug.Log("baitZoneis " + gameObject.name + " is " + baitZone);
+    }
+
+
+    public void Use()
+    {
+        Debug.Log("you have pressed use lol");
+    }
+
+}
