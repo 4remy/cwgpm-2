f6e7fbeb35830d96c54698a569a2a3c36aa7809f
Nervana <jeremyboydell@gmail.com>
2021-09-11 15:57:29 -0700

    hemoshake and trash counter
    hemoshake convo completed bool and trash cleaned away bool are now stored in the game save manager - make sure this carries over to other scenes

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/trash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//a pot breaks when you press the space key.
+
+public class trash : Interactable
+{
+
+
+    //override interact, to say what happens during an interaction for this object
+
+    protected override void Interact()
+    {
+        Debug.Log("interacting");
+        StartCoroutine(BreakCo());
+    }
+
+    //this coroutine is an add the action that interact triggers
+
+    private IEnumerator BreakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/trashCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trashCounter : MonoBehaviour
+{
+    int numberOfTrash;
+    public bool trashCleaned;
+    public BoolValue storedCleaned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //there is probably a better way to do this than a constant update.
+    //also consider putting a sound effect on the trash script
+    // Update is called once per frame
+    void Update()
+    {
+        numberOfTrash = GameObject.FindGameObjectsWithTag("trash").Length;
+
+        if (numberOfTrash == 0)
+        {
+            trashCleaned = true;
+            storedCleaned.RuntimeValue = trashCleaned;
+          //  Debug.Log("trashCleaned " + " is " + trashCleaned);
+        }
+        else
+        {
+            trashCleaned = false;
+           // Debug.Log("trashCleaned " + " is " + trashCleaned);
+        }
+    }
+}
