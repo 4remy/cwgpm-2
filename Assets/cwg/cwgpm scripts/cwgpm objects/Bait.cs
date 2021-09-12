bcf62ff75fb6c75cd89d69a3469f9c7996fdb46c
Nervana <jeremyboydell@gmail.com>
2021-09-12 00:36:47 -0700

    baitzone triggers fixed
    child triggers work correctly. maybe clean up code/names of stuff for the hand

--- Assets/cwg/cwgpm scripts/cwgpm objects/Bait.cs
+++ Assets/cwg/cwgpm scripts/cwgpm objects/Bait.cs
@@ -11,24 +11,59 @@ public class Bait : MonoBehaviour
 
     //i think interact isnt working because of the triggers
 
+    [Header("Animation")]
+    private Animator animator;
+
     public Signal BaitSignal;
     public bool baitZone;
+
+    private bool trapped;
+
+    public GameObject bait;
+
+
+
+    void Awake()
+    {
+        animator = gameObject.transform.GetChild(0).GetComponent<Animator>();
+        bait.SetActive(false);
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && baitZone)
         {
-            Debug.Log("you have interacted lol");
+           if(!trapped)
+            {
+                Debug.Log("you have interacted lol");
+            }
+            else
+            {
+                Debug.Log("now is the Time");
+            }
+
         }
+    }
+
+    public void ChildTrigger()
+    {
+        Debug.Log("general zone triggered");
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "baitChild")
+        {
+            return; // do nothing
+        }
+        else
+        {
             Debug.Log("player in bait zone");
             baitZone = true;
-        Debug.Log("baitZone " + " is " + baitZone);
-
+            Debug.Log("baitZone " + " is " + baitZone);
+        }
     }
+
 
     public void OnTriggerExit2D(Collider2D other)
     {
@@ -36,6 +71,7 @@ public class Bait : MonoBehaviour
             baitZone = false;
             Debug.Log("baitZone" + " is " + baitZone);
     }
+
 
 
     public void Use()
@@ -59,7 +95,28 @@ public class Bait : MonoBehaviour
         {
             Debug.Log("signal recieved");
             Debug.Log("Dana voice: lets goo");
+            StartCoroutine(baitedCo());
         }
 
+    }
+
+  IEnumerator baitedCo()
+    {
+        Debug.Log("baited coroutine");
+        bait.SetActive(true);
+        animator.SetBool("return", true);
+        yield return new WaitForSeconds(2f);
+        animator.SetBool("return", false);
+        animator.SetBool("grab", true);
+        trapped = true;
+
+        yield return new WaitForSeconds(5f);
+        trapped = false;
+        bait.SetActive(false);
+        animator.SetBool("grab", false);
+        animator.SetBool("flee", true);
+        yield return new WaitForSeconds(2f);
+        animator.SetBool("flee", false);
+        //go to 'flee idle'
     }
 }
---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/baitChild.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baitChild : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (tag == "bait")
+        {
+            return; // do nothing
+        }
+        else
+        {
+            gameObject.GetComponentInParent<Bait>().ChildTrigger();
+            Debug.Log("main zone triggered!");
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("main zone left!");
+    }
+}
