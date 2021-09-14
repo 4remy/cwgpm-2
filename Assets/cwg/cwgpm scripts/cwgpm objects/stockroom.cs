c3c5aa7517e5ff273bf316c7b714611d8ba8e67f
Nervana <jeremyboydell@gmail.com>
2021-09-14 16:56:24 -0700

    stockroom script good
    gives you 5 items when you don't have any

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/stockroom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stockroom : Interactable
+{
+    [SerializeField] private Schwer.ItemSystem.Item item = default;
+    public int increaseBy;
+    public string soundEffectToPlay;
+
+    protected override void Interact()
+    {
+        if (player.inventory[item] == 0)
+        {
+            Debug.Log("damn u ran out of the item");
+            Debug.Log("i give");
+            player.inventory[item] += increaseBy;
+            AudioManager.Instance.Play(soundEffectToPlay);
+            StartCoroutine(itemCo());
+
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    IEnumerator itemCo()
+    {
+        player.RaiseItem(item);
+        yield return new WaitForSeconds(0.5f);
+         player.RaiseItem(null);
+
+    }
+}
