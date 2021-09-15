56f5a168ff2d55e6bffba2a4a43b448932554b03
Nervana <jeremyboydell@gmail.com>
2021-09-14 17:19:37 -0700

    dude burger is unlocked correctly
    when a boolvalue is completed, a thing can be hidden or revealed. it is best to pick a boolvalue that is turned on in the opposing scene, so it loads correctly when you change scenes.

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/unlockObjects.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unlockObjects : MonoBehaviour
+{
+    public BoolValue priorEvent;
+    private bool priorSuccess;
+    public GameObject unlockMe;
+    public GameObject hideMe;
+
+    //maybe have the active gameobjects as like, the child of one empty object thats like 'unlockable ' so you can turn it on and off easy 
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        priorSuccess = priorEvent.RuntimeValue;
+        if (!priorSuccess)
+        {
+            Debug.Log("not unlocked");
+            unlockMe.SetActive(false);
+            return;
+        }
+        else
+        {
+            //do thing here
+            unlockMe.SetActive(true);
+            hideMe.SetActive(false);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
