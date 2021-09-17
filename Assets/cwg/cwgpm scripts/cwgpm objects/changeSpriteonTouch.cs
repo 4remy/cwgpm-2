c5f5f9b561c04e61a859605a049d3ea05905ef4c
Nervana <jeremyboydell@gmail.com>
2021-09-16 18:51:46 -0700

    backroom door
    gotta fix

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/changeSpriteonTouch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeSpriteonTouch : MonoBehaviour
+
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.GetComponent<CharacterController2D>();
+        if (player != null)
+        {
+            return;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprite2;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var player = other.GetComponent<CharacterController2D>();
+        if (player != null)
+        {
+            return;
+        }
+        else
+        {
+            spriteRenderer.sprite = sprite1;
+        }
+
+    }
+
+
+
+}
---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/unlockedDoor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class unlockedDoor : Interactable
+{
+    [Header("Door variables")]
+    public DoorType thisDoorType;
+    public bool open;
+
+
+    public BoxCollider2D physicsCollider;
+
+    [Header("Animation")]
+    private Animator animator;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (open)
+        {
+            animator.SetBool("Open", true);
+            physicsCollider.enabled = false;
+        }
+
+
+    }
+
+    protected override void Interact()
+    {
+            if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+            {
+                Debug.Log("open door called!");
+                OpenDoor();
+            }
+        else
+        {
+            return;
+        }
+       
+    }
+
+    public void OpenDoor()
+    {
+
+        //change to different sprite
+        animator.SetBool("Open", true);
+        Debug.Log("door Opened");
+
+        //the door needs an animator for open
+        open = true;
+        //the box collider block is diabled
+        physicsCollider.enabled = false;
+    }
+
+
+
+
+
+}
