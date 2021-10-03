edc8b764e4d9f1ea483973d94af71ab3bb0cadba
Nervana <jeremyboydell@gmail.com>
2021-10-02 20:17:25 -0700

    I do Not Understand the Timeline Manager
    schwer please help

---
+++ Assets/cwg/Timeline/TimelineManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManager : MonoBehaviour
+{
+    public PlayableDirector director;
+    public bool firstPlay;
+    public BoolValue storedFirstPlay;
+
+    // Start is called before the first frame update
+    void awake()
+    {
+        Debug.Log("the timeline manager is awake btw.");
+
+        if (!firstPlay) //return true if the key exist
+        {
+            Debug.Log("First time in the game.");
+            firstPlay = true;
+            storedFirstPlay.RuntimeValue = firstPlay;
+            StartTimeline();
+        }
+        else
+        {
+            Debug.Log("It is not the first time in the game.");
+           
+            //director.Stop();
+            return;
+        }
+
+    }
+
+    public void StartTimeline()
+    {
+        director = GetComponent<PlayableDirector>();
+        director.Play();
+        print("playable director is playing");
+
+    }
+
+
+
+}
