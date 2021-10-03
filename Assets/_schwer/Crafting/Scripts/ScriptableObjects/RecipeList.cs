3201069fc15f476c27232e9a81c80e5b8cd5406f
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-10-03 17:05:00 +1100

    Create `RecipeList` and test ScriptableObject

---
+++ Assets/_schwer/Crafting/Scripts/ScriptableObjects/RecipeList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Schwer.ItemSystem {
+    [CreateAssetMenu(menuName = "Item System/Crafting Recipe List")]
+    public class RecipeList : ScriptableObject {
+        [SerializeField] private List<Recipe> _recipes = new List<Recipe>();
+        public List<Recipe> recipes => _recipes;
+    }
+}
