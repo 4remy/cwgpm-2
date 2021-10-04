e7e3225c258d27086a57cce319d71846979b5a4c
Nervana <jeremyboydell@gmail.com>
2021-10-04 03:46:05 -0700

    Revert "Cutscene plays no matter what"
    This reverts commit 161559d95a722efe95c2a4ea6fec6250e3ffec51.

--- Assets/cwg/Timeline/TimelineManager.cs
+++ Assets/cwg/Timeline/TimelineManager.cs
@@ -8,14 +8,10 @@ public class TimelineManager : MonoBehaviour
     public PlayableDirector director;
     public bool firstPlay;
     public BoolValue storedFirstPlay;
-    private bool sceneSkipped = true;
-    private float timeToSkipTo;
 
     // Start is called before the first frame update
-    void Start()
+    void awake()
     {
-        sceneSkipped = false;
-        firstPlay = storedFirstPlay.RuntimeValue;
         Debug.Log("the timeline manager is awake btw.");
 
         if (!firstPlay) //return true if the key exist
@@ -23,26 +19,25 @@ public class TimelineManager : MonoBehaviour
             Debug.Log("First time in the game.");
             firstPlay = true;
             storedFirstPlay.RuntimeValue = firstPlay;
-            director = GetComponent<PlayableDirector>();
-            director.Play();
+            StartTimeline();
         }
         else
         {
             Debug.Log("It is not the first time in the game.");
-            director.Stop();
-            sceneSkipped = true;
-            director.time = 90f;
-            // this.gameObject.SetActive(false);
+           
+            //director.Stop();
             return;
         }
 
     }
 
-    public void GetSkipTime(float skipTime)
+    public void StartTimeline()
     {
-        timeToSkipTo = skipTime;
-    }
+        director = GetComponent<PlayableDirector>();
+        director.Play();
+        print("playable director is playing");
 
+    }
 
 
 
