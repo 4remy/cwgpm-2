b8d86123fb17cac5db51b414d88fa7b1f3844e42
Nervana <jeremyboydell@gmail.com>
2021-10-13 05:10:37 -0700

    shake machine improved
    it gives out the shakes, it gives you 5 at the time, idk it works good enough.

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/shakeMachine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shakeMachine : Interactable
+{
+    [SerializeField] private Schwer.ItemSystem.Item item = default;
+    public int increaseBy;
+    public string soundEffectToPlay;
+    public string soundEffectToPlay2;
+
+
+    public Signal ConvoStartSignal;
+    public Signal ConvoFinishSignal;
+
+    protected override void Interact()
+    {
+        if (!playerInRange)
+        {
+            return;
+        }
+
+        if (player.inventory[item] < 5 && playerInRange)
+        {
+            Debug.Log("damn u ran out of the item");
+            Debug.Log("i give");
+            player.inventory[item] += increaseBy;
+
+            StartCoroutine(itemCo());
+
+        }
+        else
+        {
+            AudioManager.Instance.Play(soundEffectToPlay2);
+            return;
+        }
+    }
+
+
+
+    IEnumerator itemCo()
+    {
+        AudioManager.Instance.Play("Conveyor");
+        yield return new WaitForSeconds(1f);
+        AudioManager.Instance.Stop("Conveyor");
+        player.RaiseItem(item);
+        yield return new WaitForSeconds(1f);
+        AudioManager.Instance.Play(soundEffectToPlay);
+        player.RaiseItem(null);
+
+    }
+
+
+
+}
