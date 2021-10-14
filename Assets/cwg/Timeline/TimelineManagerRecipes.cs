3933746fa1378b69f7138f28a8d9e7a3a4cc3862
Nervana <jeremyboydell@gmail.com>
2021-10-14 02:07:23 -0700

    Dog visitor cutscene
    it's looking like it's working nicely. it was too hard to animate a cutscene with dialogue, so I just created a one off conversation. it worked better.

---
+++ Assets/cwg/Timeline/TimelineManagerRecipes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineManagerRecipes : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    [SerializeField] private IntListSO discoveredRecipes = default;
+    private bool milestone1;
+
+    public int magicNumber1;
+
+
+        void Start()
+    {
+        var recipeCount = discoveredRecipes.ints.Count;
+        if (recipeCount >= magicNumber1)
+        {
+            milestone1 = true;
+        }
+        else
+        {
+            milestone1 = false;
+        }
+        if (!milestone1)
+        {
+            Debug.Log("Milestone " + gameObject.name + " is " + milestone1);
+            return;
+        }
+        else
+        {
+            Debug.Log("Milestone " + gameObject.name + " is " + milestone1);
+            director = GetComponent<PlayableDirector>();
+            director.Play();
+        }
+
+    }
+
+}
