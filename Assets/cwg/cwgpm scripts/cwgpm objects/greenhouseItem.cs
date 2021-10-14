fd59a57e158e0bce99465598b2245e250b9b733b
Nervana <jeremyboydell@gmail.com>
2021-10-13 23:31:55 -0700

    random greenhouse vegetable
    proud of this one

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/greenhouseItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class greenhouseItem : MonoBehaviour
+{
+
+
+    [Header("Random GameObjects")]
+    public GameObject[] myObjects;
+    private bool randomItemChanged = false;
+
+    public GameObject chosenItem;
+
+
+
+    void Awake()
+    {
+        chosenItem = myObjects[UnityEngine.Random.Range(0, myObjects.Length)];
+
+        chosenItem.SetActive(true);
+
+        randomItemChanged = true;
+    }
+
+}
