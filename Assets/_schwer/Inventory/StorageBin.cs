7710a0172f6b3bc66439aeca4f45db2ed0d6054d
Schwer <42115696+itsschwer@users.noreply.github.com>
2021-10-26 21:53:45 +1100

    Create `StorageBin.cs`

---
+++ Assets/_schwer/Inventory/StorageBin.cs
@@ -0,0 +1,15 @@
+﻿using Schwer.ItemSystem;
+using UnityEngine;
+
+public class StorageBin : Interactable
+{
+    [SerializeField] private InventorySO storage = default;
+
+    protected override void Interact()
+    {
+        if (player.state != CharacterController2D.State.Interact)
+        {
+            InventoryStorageManager.Request(player.inventory, storage.value);
+        }
+    }
+}
