5015492d325f3a35e1cf81caf0c2a6e78b10c4dd
Nervana <jeremyboydell@gmail.com>
2021-11-04 00:16:07 -0700

    egg minigame
    separate scene

---
+++ Assets/Minigame/destroyedByWall.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyedByWall : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 3f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Pizza>())
+        {
+            Debug.Log("landed on pizza");
+            //rb.velocity = Vector2.zero;
+            moveSpeed = 0f;
+
+        }
+
+        if (collision.GetComponent<Wall>())
+        {
+            Debug.Log("egg destroyed");
+            Destroy(this.gameObject);
+
+        }
+
+    }
+      /*
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Pizza>())
+        {
+            Debug.Log("landed on pizza");
+            rb.velocity = Vector2.zero;
+
+        }
+    }*/
+}
---
+++ Assets/Minigame/toppingFall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toppingFall : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("egg clicked 1");
+            //transform.parent = null;
+            //rb.bodyType = RigidbodyType2D.Dynamic;
+            //rb.WakeUp();
+
+            rb = GetComponent<Rigidbody2D>();
+            //rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
+
+        }
+
+        
+    }
+
+}
