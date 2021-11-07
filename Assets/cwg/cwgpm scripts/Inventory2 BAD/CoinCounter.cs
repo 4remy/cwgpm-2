63df9e4b1f32ac7687d83cd5e2e1bbeb2d2ad10b
Nervana <jeremyboydell@gmail.com>
2021-11-06 22:42:35 -0700

    Coin counter issues
    cleaned up old script for coin counter, still won't register in new scene.. weird.

--- Assets/cwg/cwgpm scripts/Inventory2 BAD/CoinCounter.cs
+++ Assets/cwg/cwgpm scripts/Inventory2 BAD/CoinCounter.cs
@@ -6,25 +6,8 @@ using UnityEngine;
 [System.Serializable]
 public class CoinCounter : ScriptableObject
 {
-    public Item currentItem;
-    public List<Item> items = new List<Item>();
-    public int numberOfKeys;
+   
     public int coins;
 
-    public void AddItem(Item itemToAdd)
-    {
-        //is the item a key
-        if(itemToAdd.isKey)
-        {
-            numberOfKeys++;
-        }
-        else
-        {
-            if(!items.Contains(itemToAdd))
-            {
-                items.Add(itemToAdd);
-            }
-        }
-    }
 
 }
