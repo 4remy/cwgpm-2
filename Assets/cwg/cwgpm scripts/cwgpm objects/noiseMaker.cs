bc428e7ca053bd67bf75b4fc6352153a01b3270f
4remy <jeremyboydell@gmail.com>
2022-05-15 15:23:44 -0700

    Noise maker added
    simple script to get a sound effect on interacting with a background item.

---
+++ Assets/cwg/cwgpm scripts/cwgpm objects/noiseMaker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// make a noise when interact
+
+public class noiseMaker : MonoBehaviour
+{
+    public bool playerInRange;
+    public string soundEffectToPlay;
+    private bool soundPlaying;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+           if(soundPlaying)
+            {
+                return;
+            }
+           else
+            {
+                StartCoroutine(NoiseCo());
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    private IEnumerator NoiseCo()
+    {
+        soundPlaying = true;
+        AudioManager.Instance.Play(soundEffectToPlay);
+        yield return new WaitForSeconds(1.3f);
+        soundPlaying = false;
+    }
+}
