14ecb2c1575abeb5c40fd7c1631359b761da2d5d
4remy <jeremyboydell@gmail.com>
2022-08-22 17:07:20 -0700

    update
    game is a bit screwy.. things like BG music not working, but working once it's closed and started again.
    the 'achievement ' stuff in cooking scene is a bit broken and I can't quite remember it. will continue to revisit

---
+++ Assets/cwg/cwgpm scripts/FlickerProximity.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerProximity : MonoBehaviour
+{
+    // this is incredibly janky and should only be used in Cobyland
+    // it actually works very well unintentionally
+    public bool playerInRange;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray;
+    public int currentSprite;
+    private bool changeTriggered;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange)
+        {
+            if(changeTriggered)
+            {
+                return; 
+            }
+            else
+            {
+                StartCoroutine(ChangeCo());
+            }
+           
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+
+
+    private IEnumerator ChangeCo()
+    {
+        changeTriggered = true;
+        spriteRenderer.sprite = spriteArray[currentSprite];
+        currentSprite++;
+
+        if (currentSprite >= spriteArray.Length)
+        {
+            currentSprite = 0;
+        }
+        yield return new WaitForSeconds(5f);
+        changeTriggered = false;
+    }
+
+}
